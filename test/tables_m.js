ca3ab97fc550636bbb3c2361eb158111b3c628a3
HorakR <rosto.horak@gmail.com>
2018-09-27 09:03:36 +0200

    Update version

---
+++ test/tables_m.js
@@ -0,0 +1,299 @@
+module.exports =
+{
+	create_user :
+	{
+		columns :
+		{
+			id   : { type: 'BIGINT:UNSIGNED', increment: true },
+			name : { type: 'VARCHAR:255' }
+		},
+		indexes :
+		{
+			primary : 'id',
+			unique  : [],
+			index   : []
+		}
+	},
+	create_user_3 :
+	{
+		columns :
+		{
+			id   : { type: 'BIGINT:UNSIGNED', increment: true },
+			name : { type: 'VARCHAR:255' }
+		},
+		indexes :
+		{
+			primary : 'id',
+			unique  : [],
+			index   : ['name']
+		}
+	},
+	errors_list :
+	{
+		columns :
+		{
+			id   : { type: 'BIGINT:UNSIGNED' },
+			name : { type: 'VARCHAR:255' }
+		},
+		indexes :
+		{
+			primary : 'id',
+			unique  : ['id,name'],
+			index   : []
+		}
+	},
+	insert_users :
+	{
+		columns :
+		{
+			id          : { type: 'BIGINT:UNSIGNED', increment: true },
+			name        : { type: 'VARCHAR:255' },
+			description : { type: 'TEXT', null: true },
+			surname     : { type: 'VARCHAR:55', null: true }
+		},
+		indexes :
+		{
+			primary : 'id',
+			unique  : ['name'],
+			index   : 'surname'
+		}
+	},
+	insert_users_2 :
+	{
+		columns :
+		{
+			id      : { type: 'BIGINT:UNSIGNED' },
+			name    : { type: 'VARCHAR:64' },
+			middle_name : { type: 'VARCHAR:64' },
+			surname : { type: 'VARCHAR:255' }
+		},
+		indexes :
+		{
+			primary : 'id,name',
+			unique  : []
+		}
+	},
+	insert_users_3 :
+	{
+		columns :
+		{
+			id      : { type: 'BIGINT:UNSIGNED' },
+			name    : { type: 'VARCHAR:255' }
+		},
+		indexes :
+		{
+			primary : 'id',
+			unique  : []
+		}
+	},
+	join_users :
+	{
+		columns :
+		{
+			id   : { type: 'BIGINT:UNSIGNED', increment: true },
+			name : { type: 'VARCHAR:255' }
+		},
+		indexes :
+		{
+			primary : 'id',
+			unique  : [],
+			index   : []
+		}
+	},
+	join_address :
+	{
+		columns :
+		{
+			id     : { type: 'BIGINT:UNSIGNED' },
+			active : { type: 'TINYINT', default: 1 },
+			city   : { type: 'VARCHAR:255' }
+		},
+		indexes :
+		{
+			primary : 'id',
+			unique  : ['city'],
+			index   : []
+		}
+	},
+	set_users :
+	{
+		columns :
+		{
+			id          : { type: 'BIGINT:UNSIGNED', increment: true },
+			name        : { type: 'VARCHAR:255' },
+			description : { type: 'TEXT', null: true },
+			created     : { type: 'TIMESTAMP', default: 'CURRENT_TIMESTAMP', update: 'CURRENT_TIMESTAMP' },
+			surname     : { type: 'VARCHAR:55', null: true }
+		},
+		indexes :
+		{
+			primary : 'id',
+			unique  : 'name',
+			index   : [ 'surname' ]
+		}
+	},
+	set_address :
+	{
+		columns :
+		{
+			id          : { type: 'BIGINT:UNSIGNED', increment: true },
+			addressID   : { type: 'BIGINT:UNSIGNED' },
+			name        : { type: 'VARCHAR:255' },
+			description : { type: 'TEXT', null: true },
+			created     : { type: 'TIMESTAMP', default: 'CURRENT_TIMESTAMP', update: 'CURRENT_TIMESTAMP' },
+			city        : { type: 'VARCHAR:55', null: true }
+		},
+		indexes : {
+			primary : 'id',
+			unique  : 'addressID,name',
+			index   : [ 'city' ]
+		}
+	},
+	set_phones :
+	{
+		columns :
+		{
+			userID : { type: 'BIGINT:UNSIGNED' },
+			phone  : { type: 'BIGINT:UNSIGNED' }
+		},
+		indexes : {
+			primary : '',
+			unique  : [ ],
+			index   : [ ]
+		}
+	},
+	table_users :
+	{
+		columns :
+		{
+			id      : { type: 'BIGINT:UNSIGNED', increment: true },
+			name    : { type: 'VARCHAR:255' },
+			surname : { type: 'VARCHAR:255', null: true, default: 'NULL' }
+		},
+		indexes :
+		{
+			primary : 'id',
+			unique  : 'name',
+			index   : [ 'surname' ]
+		}
+	},
+	table_address :
+	{
+		columns :
+		{
+			id   : { type: 'BIGINT:UNSIGNED', increment: true },
+			name : { type: 'VARCHAR:255' },
+			city : { type: 'VARCHAR:255', null: true, default: 'NULL' }
+		},
+		indexes :
+		{
+			primary : null,
+			unique  : [ 'name' ],
+			index   : [ 'id' ]
+		}
+	},
+	table_cities :
+	{
+		columns :
+		{
+			id   : { type: 'BIGINT:UNSIGNED', increment: true },
+			name : { type: 'VARCHAR:255' },
+			city : { type: 'VARCHAR:255', null: true, default: 'NULL' }
+		},
+		indexes :
+		{
+			primary : null,
+			unique  : 'name',
+			index   : [ 'id' ]
+		}
+	},
+	test_users :
+	{
+		columns : {
+			id          : { type: 'BIGINT:UNSIGNED', increment: true },
+			name        : { type: 'VARCHAR:255', null: true },
+			description : { type: 'TEXT', null: true },
+			created     : { type: 'TIMESTAMP', default: 'CURRENT_TIMESTAMP', update: 'CURRENT_TIMESTAMP' },
+			surname     : { type: 'VARCHAR:55', null: true, default: 'NULL' }
+		},
+		indexes : {
+			primary : 'id',
+			unique  : [],
+			index   : [ 'name' ]
+		}
+	},
+	union_users :
+	{
+		columns :
+		{
+			id   : { type: 'BIGINT:UNSIGNED', increment: true },
+			name : { type: 'VARCHAR:255' }
+		},
+		indexes :
+		{
+			primary : 'id',
+			unique  : [],
+			index   : []
+		}
+	},
+	union_address :
+	{
+		columns :
+		{
+			id     : { type: 'BIGINT:UNSIGNED', increment: true },
+			street : { type: 'VARCHAR:255' },
+			city   : { type: 'VARCHAR:255' }
+		},
+		indexes :
+		{
+			primary : 'id',
+			unique  : [],
+			index   : []
+		}
+	},
+	update_users :
+	{
+		columns :
+		{
+			id    : { type: 'BIGINT:UNSIGNED' },
+			uid   : { type: 'BIGINT:UNSIGNED' },
+			name  : { type: 'VARCHAR:255' }
+		},
+		indexes :
+		{
+			primary : 'id',
+			unique  : [ 'uid' ],
+			index   : []
+		}
+	},
+	update_users_2 :
+	{
+		columns :
+		{
+			id      : { type: 'BIGINT:UNSIGNED' },
+			'u-id'  : { type: 'BIGINT:UNSIGNED' },
+			name    : { type: 'VARCHAR:255' }
+		},
+		indexes :
+		{
+			primary : '',
+			unique  : [],
+			index   : []
+		}
+	},
+	update_users_3 :
+	{
+		columns :
+		{
+			name    : { type: 'VARCHAR:255' },
+			surname : { type: 'VARCHAR:255' },
+			city 	: { type: 'VARCHAR:255' }
+		},
+		indexes :
+		{
+			primary : '',
+			unique  : [ 'name,surname' ],
+			index   : []
+		}
+	}
+};
--- test/tests/database.js
+++ test/tests/database.js
@@ -2,7 +2,8 @@
 
 const assert = require('assert');
 const SQLError = require( '../../lib/errors.js');
-const tables = require('../tables.js')
+const tables = require('../tables.js');
+const modiefied_tables = require('../tables_m.js')
 
 const SQL = new (require('../../lib/sql.js'))(
 {
@@ -11,7 +12,7 @@ const SQL = new (require('../../lib/sql.js'))(
 		host     : 'localhost',
 		user     : 'root',
 		password : '',
-		database : 'test'
+		database : 'test_1'
 	}
 });
 
@@ -20,8 +21,22 @@ let insert, select, delete_row;
 it( 'Create', async() =>
 {
 	let cnt = 0, database = 'test_1';
+	//console.log('DB', await SQL.query().create_database( database, tables ));
 	let test = await SQL.query().create_database( database, tables, { result_type: 'array' } );
+
+	assert.ok( test.create && test.create.length === 23 , 'Test error '+( ++cnt )+' failed ' + JSON.stringify( test, null, '  ' ) );
+
+}).timeout(100000);
+
+/*
+it( 'Modify', async() =>
+{
+	let cnt = 0;
+	let test = await SQL.query().modify_database( modiefied_tables, { result_type: 'array', drop_table: true } );
+
+	console.log( 'test', test );
 
 	assert.ok( test.create && test.create.length === 22 , 'Test error '+( ++cnt )+' failed ' + JSON.stringify( test, null, '  ' ) );
 
 }).timeout(100000);
+*/
